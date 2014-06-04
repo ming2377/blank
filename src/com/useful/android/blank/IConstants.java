99034f4511981ea0c6d7c6453b6c5c89a25109e2
ming2198 <mlu27768@gmail.com>
2014-06-04 15:01:24 -0700

    Initial index

---
+++ src/com/useful/android/blank/IConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.useful.android.blank;
+
+/**
+ * @author Michael
+ *
+ */
+public interface IConstants {
+
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+}
